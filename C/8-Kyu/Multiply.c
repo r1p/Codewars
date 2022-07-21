386510347c934b34f201499e1971019007d0625f
Álex B <Leex@protonmail.com>
2022-07-21 10:27:45 +0200

    Create Multiply.c

---
+++ C/8-Kyu/Multiply.c
@@ -0,0 +1,14 @@
+/*
+Description:
+This code does not execute properly. Try to figure out why.
+  
+Categories:
+#Debugging #Fundamentals
+
+Solve date:
+20 Jul 2021
+*/
+
+int multiply(int a, int b) {
+  return a*b;
+}
