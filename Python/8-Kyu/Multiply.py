ea5e0c2f2f74921b0734f89a4b0d1edfcfdcbe5a
Álex B <Leex@protonmail.com>
2022-07-21 09:59:29 +0200

    Create Multiply.py

---
+++ Python/8-Kyu/Multiply.py
@@ -0,0 +1,13 @@
+'''
+Description:
+This code does not execute properly. Try to figure out why.
+
+Categories:
+#Debugging #Fundamentals
+
+Solve date:
+20 Jul 2021
+'''
+
+def multiply(a, b):
+  return a*
