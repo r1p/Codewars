93e2a1ca7da9f22afea2c45c1b8fcd150336456e
Álex B <Leex@protonmail.com>
2022-07-21 10:30:31 +0200

    Update Vowel_Count.py

--- Python/7-Kyu/Vowel_Count.py
+++ Python/7-Kyu/Vowel_Count.py
@@ -5,7 +5,7 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 
 Categories:
-#Debugging #Fundamentals
+#Strings #Fundamentals
 
 Solve date:
 07 Feb 2022 
