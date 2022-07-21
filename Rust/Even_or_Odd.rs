7bf38a9da9b51b382eeeb7549f3287515aad1558
Álex B <Leex@protonmail.com>
2022-07-21 10:35:15 +0200

    Create Even_or_Odd.rs

---
+++ Rust/Even_or_Odd.rs
@@ -0,0 +1,18 @@
+/*
+Description:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+Categories:
+#Mathematics #Fundamentals
+
+Solve date:
+26 Jun 2022
+*/
+
+fn even_or_odd(i: i32) -> &'static str {
+    if i % 2 == 0 {
+        return "Even";
+    } else {
+        return  "Odd";
+    }
+}
