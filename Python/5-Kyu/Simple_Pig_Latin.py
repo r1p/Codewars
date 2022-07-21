896a3f2d7d2aa3a9af0c071aeb58313e7ce4822d
Álex B <Leex@protonmail.com>
2022-07-21 10:20:32 +0200

    Create Simple_Pig_Latin.py

---
+++ Python/5-Kyu/Simple_Pig_Latin.py
@@ -0,0 +1,24 @@
+'''
+Description:
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples:
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+
+Categories:
+#Regular Expressions #Algorithms
+
+Solve date:
+21 Jul 2021 
+'''
+
+def pig_it(text:str) -> str:
+    result = ""
+    text_list = text.split()
+    for x in text_list:
+        if x.isalpha():
+            result += x[1:]+x[0] + "ay " # Move the first to the last position and append "ay "
+        else:
+            result += x + " "
+    return result[:-1] # Remove the last char
