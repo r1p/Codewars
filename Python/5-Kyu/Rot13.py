fcfab96b3b1b047f7a46cd2222a0d2fe6bb59e75
Álex B <Leex@protonmail.com>
2022-07-22 12:04:27 +0200

    Create Rot13.py

---
+++ Python/5-Kyu/Rot13.py
@@ -0,0 +1,34 @@
+'''
+Description:
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+Please note that using encode is considered cheating.
+
+Categories:
+#Ciphers #Fundamentals
+
+Solve date:
+21 Jul 2022
+'''
+
+def rot13(message):
+    
+    dict = {
+    'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q', 'e': 'r', 'f': 's', 'g': 't', 'h': 'u', 'i': 'v', 'j': 'w', 'k': 'x', 'l': 'y', 'm': 'z',
+    'n': 'a', 'o': 'b', 'p': 'c', 'q': 'd', 'r': 'e', 's': 'f', 't': 'g', 'u': 'h', 'v': 'i', 'w': 'j', 'x': 'k', 'y': 'l', 'z': 'm',
+    'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q', 'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U', 'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y', 'M': 'Z',
+    'N': 'A', 'O': 'B', 'P': 'C', 'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G', 'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K', 'Y': 'L', 'Z': 'M',
+    }
+
+    messageList = []
+    messageList[:0]+=message
+
+    rot13 = ""
+
+    for x in messageList:
+        if x in dict:
+            rot13 += (dict[x])
+        else:
+            rot13 += (x)
+    
+    return rot13
